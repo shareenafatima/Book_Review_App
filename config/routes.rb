2f3c95bf126d729523f7ad69c8a79d5f192aec58
shareena fatima <shareenafatima40@gmail.com>
2024-04-15 23:21:30 +0500

    setup user database and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controller: {
+    registrations: 'registrations'
+  }
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
