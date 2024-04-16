172dfc664630d92a4b562ae5d2fdbcbde19f530b
shareena fatima <shareenafatima40@gmail.com>
2024-04-16 15:25:45 +0500

    set up security for edit and add book

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,5 @@
 module BooksHelper
+  def book_author(book)
+    user_signed_in? && current_user.id == book.user_id
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,3 @@
 class Book < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
 end
