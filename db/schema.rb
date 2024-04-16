1b0387ce4522ebe996a7235e1953bc791b9e8cd8
shareena fatima <shareenafatima40@gmail.com>
2024-04-17 01:54:29 +0500

    set up security for edit and add book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_16_195555) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_16_202543) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_16_195555) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "category_id"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
