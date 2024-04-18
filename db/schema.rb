ca9ed34ddaf7f2a53065f46d8413f5e642a8245d
shareena fatima <shareenafatima40@gmail.com>
2024-04-19 02:13:35 +0500

    add book_id and user_id to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_210647) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_211213) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -63,6 +63,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_210647) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "book_id"
   end
 
   create_table "users", force: :cascade do |t|
