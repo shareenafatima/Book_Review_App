40af6cae4680a95021d11956dfdc00b95c2e9de3
shareena fatima <shareenafatima40@gmail.com>
2024-04-19 02:08:57 +0500

    migrate review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_17_165214) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_210647) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -54,6 +54,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_17_165214) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
